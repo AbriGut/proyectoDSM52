ca95f3e28a840c1224b5159d3dae13f770a765b6
AbriGut <abiizgnunez@gmail.com>
2023-02-15 17:28:33 -0600

    Tab button iconos

---
+++ src/Screens/SettingsScreen.js
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native';
+export const SettingsScreen=()=> {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings Screen!</Text>
+    </View>
+    );
+  }
